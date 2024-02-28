d14a17379d3ca916354e2d0b99addf399c01f03f
ShouMajker <kamilekkrawczyk404@gmail.com>
2024-02-28 17:07:07 +0100

    fix: hero images path

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -58,7 +58,7 @@ export const Hero = ({
           <img
             key={index}
             ref={(element) => (imagesRefs.current[index] = element)}
-            src={`/hero/${image}`}
+            src={`./hero/${image}`}
             className={
               "hero-image absolute top-0 left-0 h-full w-full " +
               (index === 0 ? "opacity-100" : "opacity-0")
